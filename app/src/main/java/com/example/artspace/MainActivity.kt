640dd4c951b4825ebc4b41557f0d1710843b8dcb
Gustavo Lopez <gushlopezg2005@gmail.com>
2026-02-24 21:56:21 -0600

    Jeje me pashe me pashe

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,4 +1,6 @@
-package com.example.artspaceimport android.os.Bundle
+package com.example.artspace
+
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
